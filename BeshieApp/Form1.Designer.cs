ff187566828c5b36cf9a1eff3556deccaa924636
Julius Glenn Espiritu <116713950+espiritujulius@users.noreply.github.com>
2023-07-06 17:12:21 +0800

    Add files via upload

---
+++ BeshieApp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BeshieApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbText = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(175, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "🤸 BESHY🤸";
+            // 
+            // tbText
+            // 
+            this.tbText.Location = new System.Drawing.Point(74, 78);
+            this.tbText.Name = "tbText";
+            this.tbText.Size = new System.Drawing.Size(406, 27);
+            this.tbText.TabIndex = 1;
+            this.tbText.TextChanged += new System.EventHandler(this.tbText_TextChanged);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(486, 78);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(27, 29);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "🗎";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblOutput.Location = new System.Drawing.Point(74, 117);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 25);
+            this.lblOutput.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(572, 224);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.tbText);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BESHY";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox tbText;
+        private Button btnGenerate;
+        private Label lblOutput;
+    }
+}
---
+++ BeshieApp/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace BeshieApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(lblOutput.Text);
+        }
+
+        private void tbText_TextChanged(object sender, EventArgs e)
+        {
+            string inputText = tbText.Text;
+
+            string[] words = inputText.Split(' ');
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            int wordCount = 0;
+            int maxWordsPerLine = 6; // NEW LINE
+
+            foreach (string word in words)
+            {
+                stringBuilder.Append("🤸" + word);
+                wordCount++;
+
+                if (wordCount % maxWordsPerLine == 0)
+                {
+                    stringBuilder.AppendLine();
+                }
+
+            }
+
+            string modifiedText = stringBuilder.ToString().Trim();
+
+            lblOutput.Text = modifiedText;
+        }
+    }
+}
